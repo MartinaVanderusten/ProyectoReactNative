86944c968e85244bb7fd65dd62dca8034ad33ec0
PacoIribarne <iribarnefrancisco@gmail.com>
2022-06-13 15:42:32 -0300

    prueba

---
+++ src/screens/Home/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+        }
+      }
+      render(){
+        return (
+          <View>
+              <Text>Hoss</Text>
+          </View>
+        )
+      }
+    }
+
+
+export default Home;
--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,15 +1,24 @@
-import { View } from 'react-native'
+import { View, FlatList } from 'react-native'
 import React, {Component} from 'react'
+import {db} from '../../firebase/config'
+
 
 class Profile extends Component {
     constructor(props){
       super(props)
       this.state = {
+        
       }
     }
     render(){
       return (
-        <View>
+        <View style={styles.flatlist}>
+          <FlatList 
+            data={}
+            keyExtractor={}
+            renderItem={}
+          
+          />
         </View>
       )
     }
